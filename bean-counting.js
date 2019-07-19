b339499d851a9a09b736d29a66ed34fde9239f83
Patty Ouwehand <pattyouwehand@gmail.com>
2019-07-19 15:41:58 +0200

    exercise sumOfRange

---
+++ bean-counting.js
@@ -0,0 +1,11 @@
+function countChar(string, char){
+    let count = 0;
+    for(let i= 0; i < string.length; i++){
+        if(string[i] === char){
+            count += 1;
+        }
+    }
+    return count;
+}
+
+console.log(countChar("bobbi", "b"))
